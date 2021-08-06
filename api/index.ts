ba73b067c8cb7a2fead340ff38ed6ae3ee83a256
maerzhase <markus@lili16.de>
2021-08-07 00:03:37 +0200

    use ISong as return type for getSongsTrending

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import ISong from '../stores/models/Song'
 
 const API_BASE_PATH = 'https://api-stg.jam-community.com'
 
@@ -6,7 +7,7 @@ const api = axios.create({
   baseURL: API_BASE_PATH,
 })
 
-export async function getSongsTrending(): Promise<any> {
+export async function getSongsTrending(): Promise<ISong[]> {
   try {
     const response = await api.get('/song/trending').then((r) => r.data)
     return response
