402f93f70d89a4432ede7357e715e9174d73b3f1
maerzhase <markus@lili16.de>
2021-08-07 10:33:52 +0200

    display song cover image; render audio player
    using the html5 audio tag to display a first
    version of an audio player for each song.
    noticed that not all songs actually have
    an audio file in the music_file_path. seems
    to be consistently wrong when no music_file_mimetype
    exist.

--- components/Song.tsx
+++ components/Song.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Artist from './Artist'
+import Image from 'next/image'
 
 import ISong from '../stores/models/Song'
 
@@ -13,7 +14,13 @@ const Song = (props: SongProps): JSX.Element => {
   return (
     <div>
       <h1>{song.name}</h1>
+      {song.music_file_mimetype}
+      <Image src={song.cover_image_path} width={200} height={200} />
       {withArtist && <Artist artist={song.artist} />}
+      <audio controls>
+        <source src={song.music_file_path} />
+        Your browser does not support the audio element.
+      </audio>
     </div>
   )
 }
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ['dz2l6nhikl0ua.cloudfront.net', 'api-stg.jam-community.com'],
+  },
+}
