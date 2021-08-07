bcedab33e8bfd3c3668082ce5a7cbe24fa2de0e9
maerzhase <markus@lili16.de>
2021-08-07 10:31:41 +0200

    include optional music_file_mimetype into ISong interface

--- stores/models/Song.ts
+++ stores/models/Song.ts
@@ -20,6 +20,7 @@ interface ISong {
   cover_image_path: string
   cover_image_aspect_ratio?: string
   music_file_path: string
+  music_file_mimetype?: string
   song_genres: Array<IGenre>
   song_release: string
   mix_packs: Array<string>
